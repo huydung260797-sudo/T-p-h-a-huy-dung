f7dd8d4dec4c66534b33af774121ebf09feaf61c
huydung260797-sudo <huydung260797@gmail.com>
2026-09-22 10:33:58 +0700

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'taphoca-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        if (response.ok && event.request.method === 'GET') {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      });
+    }).catch(() => {
+      return caches.match('./index.html');
+    })
+  );
+});
